25260732e41654636c46198faf2266fc4d7a7cf2
Agnes <51322389+yuchen0531@users.noreply.github.com>
2022-01-04 14:27:30 +0800

    Add files via upload

---
+++ precache-manifest.6fb2df1a126798dd28a5bd8313ddf832.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936ed277c356d1e385f6",
+    "url": "/pwatest/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwatest/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "946ab556a998d77da55fad9a1d1c087a",
+    "url": "/pwatest/index.html"
+  },
+  {
+    "revision": "c5702ee09dcb09c8acfc",
+    "url": "/pwatest/js/about.c2002408.js"
+  },
+  {
+    "revision": "936ed277c356d1e385f6",
+    "url": "/pwatest/js/app.e22da128.js"
+  },
+  {
+    "revision": "a6e76fc19938578a9ce2",
+    "url": "/pwatest/js/chunk-vendors.96c3771f.js"
+  },
+  {
+    "revision": "d2a69d87bba02a62d5fc5c6a9a123ca1",
+    "url": "/pwatest/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwatest/robots.txt"
+  }
+]);
